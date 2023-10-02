653055def326ec39267a32c9139897870a50e5d7
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-10-01 18:18:39 -0700

    Remove action

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -5,6 +5,7 @@ function TableHeader() {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Action</th>
       </tr>
     </thead>
   );
@@ -14,25 +15,38 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
+        <td>{
+          row.name
+        }</td>
+        <td>{
+          row.job
+        }</td>
+        <td>
+          <button onClick={
+            () => props.removeCharacter(index)
+          }>
+            Delete
+          </button>
+        </td>
       </tr>
     );
-   }
-  );
+  });
   return (
-      <tbody>
-        {rows}
-       </tbody>
-   );
+    <tbody> {rows} </tbody>
+  );
 }
 
 function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+  return (
+    <table>
+      <TableHeader/>
+      <TableBody characterData={
+          props.characterData
+        }
+        removeCharacter={
+          props.removeCharacter
+        }/>
+    </table>
+  );
 }
 export default Table;
